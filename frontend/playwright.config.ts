1f1ef0a98a695abf5b1731d768e66bf5099c9ec2
TiagoMadeira <tiagopmadeira@gmail.com>
2026-05-15 16:28:58 +0100

    use https on tests instead

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'http://localhost:32080',
+    baseURL: 'http://localhost:32443',
     browserName: 'chromium',
     headless: true,
   },
