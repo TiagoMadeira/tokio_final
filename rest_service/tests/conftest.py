5b10ff4224a9cc12616ceb9a256ef9473d0809d8
TiagoMadeira <tiagopmadeira@gmail.com>
2026-05-18 15:45:50 +0100

    create ssl context for local integration testing

--- rest_service/tests/conftest.py
+++ rest_service/tests/conftest.py
@@ -2,6 +2,7 @@ import sys;sys.path.append('.')
 from app.main import app
 import pytest
 import httpx
+import ssl
 import os
 from app.dependencies import verify_token
 from tests.dependencies import test_verify_token
@@ -14,7 +15,9 @@ def skip_auth():
 
 @pytest.fixture
 def api_client():
-    cert_path = "/etc/tls/tls.crt"
-    ssl_verification = cert_path if os.path.exists(cert_path) else True
-    with httpx.Client(verify=ssl_verification) as client:
+    # Create an SSL context for local integration testing
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    with httpx.Client(verify=ssl_context) as client:
         yield client
