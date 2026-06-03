618c8ee1f7bc3e8f4539c084b069191190eeb3d0
TiagoMadeira <tiagopmadeira@gmail.com>
2026-06-03 18:38:04 +0100

    fix tests

--- rest_service/app/clients/post_service.py
+++ rest_service/app/clients/post_service.py
@@ -7,7 +7,7 @@ from app.schemas.auth import authRegister
 from app.schemas.posts import postCreate, postUpdate
 from app.utils import auth_header, handle_responses, json_header
 
-POSTS_BASE_URL = "{0}/posts/".format(settings.POST_SERVICE_URL)
+POSTS_BASE_URL = "{0}/posts".format(settings.POST_SERVICE_URL)
 
 def create_post(data :postCreate, token):
     res = requests.post("{0}/post".format(settings.POST_SERVICE_URL), data=data.model_dump_json(), headers = json_header() | auth_header(token))
@@ -18,14 +18,14 @@ def create_post(data :postCreate, token):
     
     
 def delete_post(post_id :int , token):
-    res = requests.delete(POSTS_BASE_URL + "{0}".format(post_id) , headers = auth_header(token) )
+    res = requests.delete(POSTS_BASE_URL + "/{0}".format(post_id) , headers = auth_header(token) )
     if res.status_code == 200:
         return res.json()
     
     handle_responses(res)
 
 def update_post(post_id :int, data :postUpdate, token):
-    res = requests.patch(POSTS_BASE_URL + "{0}".format(post_id) , data=data.model_dump_json(), headers = json_header() | auth_header(token) )
+    res = requests.patch(POSTS_BASE_URL + "/{0}".format(post_id) , data=data.model_dump_json(), headers = json_header() | auth_header(token) )
     if res.status_code == 200:
         return res.json()
     
@@ -50,7 +50,7 @@ def get_posts_by_author(author: str):
     if not re.match(r"^[a-zA-Z0-9\-]+$", author):
         raise BadRequestException(detail="Only alfanumeric characters allowed for author name")
 
-    res = requests.get(POSTS_BASE_URL + author)
+    res = requests.get(POSTS_BASE_URL + "/{0}".format(author))
     if res.status_code == 200:
         return res.json()
    
