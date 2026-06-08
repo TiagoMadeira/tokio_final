1b46c69e422352d96a2c56220b898d33bce97551
TiagoMadeira <tiagopmadeira@gmail.com>
2026-06-08 13:38:44 +0100

    update sonarQube properties and frontend coverage config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -2,6 +2,11 @@ import {defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig ( {
     test: {
+        coverage: {
+            provider: 'v8', // or 'istanbul'
+            reporter: ['text', 'lcov'], // 'lcov' is mandatory for SonarCloud
+            reportsDirectory: './coverage' // This maps to frontend/coverage/
+        },
         environment: 'jsdom',
         exclude: [...configDefaults.exclude, '**/e2e/**'],
     }
